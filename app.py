68b29ec7fb1574e6a76cf167a7d72918fdd85940
sterbini <guido.sterbini@cern.ch>
2020-04-21 13:43:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-import os
-from flask import Flask
-app = Flask(__name__)
-@app.route('/')
-def home():
-    """Render website's home page."""
-    return 'Hello World!'
-if __name__ == '__main__':
-    app.run(debug="True")
+import streamlit as st
+st.write('Hello, world!!')
+
+#import os
+#from flask import Flask
+#app = Flask(__name__)
+#@app.route('/')
+#def home():
+#    """Render website's home page."""
+#    return 'Hello World!'
+#if __name__ == '__main__':
+#    app.run(debug="True")
     
-#import streamlit as st
-#st.write('Hello, world!!')
+
